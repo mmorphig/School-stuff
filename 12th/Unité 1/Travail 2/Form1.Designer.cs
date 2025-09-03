0c8aae99eec5f99a0b81fd1a68432fb2c73515e7
mmorphig <mmorphig@proton.me>
2025-09-03 11:44:55 -0400

    Another assignment done.

--- 12th/Unité 1/Travail 2/Form1.Designer.cs
+++ 12th/Unité 1/Travail 2/Form1.Designer.cs
@@ -28,21 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Nom = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Age = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DN = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DE = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Nom,
+            this.Age,
+            this.DN,
+            this.DE});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 426);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Nom
+            // 
+            this.Nom.Text = "Nom";
+            this.Nom.Width = 193;
+            // 
+            // Age
+            // 
+            this.Age.Text = "Age";
+            this.Age.Width = 193;
+            // 
+            // DN
+            // 
+            this.DN.Text = "Date de naissance";
+            this.DN.Width = 193;
+            // 
+            // DE
+            // 
+            this.DE.Text = "Date d\'enregistement";
+            this.DE.Width = 193;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Deuxième ListView";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Nom;
+        private System.Windows.Forms.ColumnHeader Age;
+        private System.Windows.Forms.ColumnHeader DN;
+        private System.Windows.Forms.ColumnHeader DE;
     }
 }
 
--- 12th/Unité 1/Travail 2/Form1.cs
+++ 12th/Unité 1/Travail 2/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +14,53 @@ namespace Travail_2
 {
     public partial class Form1 : Form
     {
+        public class Etudiant
+        {
+            public string nom;
+            public uint age;
+            public DateTime dateNaissance;
+            public DateTime dateEnregistement;
+            public Etudiant(string nom, uint age, DateTime dateNaissance)
+            {
+                this.nom = nom;
+                this.age = age;
+                this.dateNaissance = dateNaissance;
+                this.dateEnregistement = DateTime.Now;
+            }
+            public Etudiant(string nom, uint age, DateTime dateNaissance, DateTime dateEnregistement)
+            {
+                this.nom = nom;
+                this.age = age;
+                this.dateNaissance = dateNaissance;
+                this.dateEnregistement = dateEnregistement;
+            }
+        }
+
+        public Etudiant[] Telecharger() 
+        {
+            Etudiant[] etudiantsTelecharge = new Etudiant[] {
+                new Etudiant("Jean Smithson", 21, new DateTime(1997, 12, 1), new DateTime(2021, 3, 10)),
+                new Etudiant("Nima Abdo", 17, new DateTime(2001, 5, 23), new DateTime(2021, 7, 25)),
+                new Etudiant("Simon Malheureux", 18, new DateTime(2000, 11, 15), new DateTime(2021, 5, 16)),
+                new Etudiant("Rita Mou", 17, new DateTime(2001, 8, 30), new DateTime(2021, 10, 25))
+            };
+            return etudiantsTelecharge;
+        }
+
+        public void Afficher(Etudiant[] etudiantsAAfficher)
+        {
+            listView1.Items.Clear();
+            foreach (Etudiant etudiant in etudiantsAAfficher)
+            {
+                listView1.Items.Add(new ListViewItem(new[] {
+                    etudiant.nom, 
+                    etudiant.age.ToString(), 
+                    etudiant.dateNaissance.ToShortDateString(), 
+                    etudiant.dateEnregistement.ToShortDateString() 
+                }));
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +68,9 @@ namespace Travail_2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-GB"); // pour avoir dd/mm/YYYY
+            Etudiant[] etudiants = Telecharger();
+            Afficher(etudiants);
         }
     }
 }
